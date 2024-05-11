5ba6e3eff76ad55859cc79a132b487f56ffe1d02
John Thompson <john@springframework.guru>
2024-05-11 14:10:18 -0400

    adding create beer

--- src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
@@ -32,6 +33,12 @@ public class RestTemplateBuilderConfig {
                 (clientRegistrationRepository, oAuth2AuthorizedClientService);
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
         return authorizedClientManager;
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder(RestTemplateBuilder restTemplateBuilder){
+
+        return RestClient.builder(restTemplateBuilder.build());
     }
 
 
